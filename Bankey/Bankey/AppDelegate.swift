4c89bd125c345ab67c3e02cd099253ef980eb2b6
e <learing000@icloud.com>
2022-08-16 15:56:19 +0400

    Add first onboarding screen

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
         //window?.rootViewController = LoginViewController()
-        window?.rootViewController = ObcViewController()
+//        window?.rootViewController = ObcViewController()
+        window?.rootViewController = OndoardingViewController()
         
         return true
     }
